ad2b96856ebb3a3e0448223bd73230ae5a6a0806
Daniel Pérez <daniel.perez@sonatasmx.com>
2019-05-21 13:12:03 -0700

    Update index.html

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/new-ng7'));
 app.get('*', function(req,res) {
  // Replace the '/dist/<to_your_project_name>/index.html'
- res.sendFile(path.join(__dirname + '/dist/new-ng7/home/index.html'));
+ res.sendFile(path.join(__dirname + '/dist/new-ng7/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
